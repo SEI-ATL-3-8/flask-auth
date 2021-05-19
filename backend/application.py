9aa4930e9aaaf65a4816bceb314ee82e3af496a1
Peter Macaluso <moxious.manoeuvre@gmail.com>
2021-05-19 12:32:17 -0400

    revert

--- backend/application.py
+++ backend/application.py
@@ -13,37 +13,6 @@ def root():
   return 'ok'
 app.route('/', methods=["GET"])(root)
 
-def signup():
-  try:
-    user = models.User(
-      email=request.json["email"],
-      password=request.json["password"]
-    )
-    models.db.session.add(user)
-    models.db.session.commit()
-    return { "user": user.to_json() }
-  except sqlalchemy.exc.IntegrityError:
-    return { "message": "email already taken" }, 400
-app.route('/users', methods=["POST"])(signup)
-
-def login():
-  user = models.User.query.filter_by(email=request.json["email"]).first()
-
-  if user.password == request.json["password"]:
-    return { "user": user.to_json() }
-  else:
-    return { "message": "login failed" }, 401
-app.route('/users/login', methods=["POST"])(login)
-
-def verify():
-  user = models.User.query.filter_by(id=request.headers["Authorization"]).first()
-  print(request.headers)
-  if user:
-    return { "user": user.to_json() }
-  else:
-    return { "message": "user not found" }, 404
-app.route('/users/verify', methods=["GET"])(verify)
-
 if __name__ == '__main__':
   port = os.environ.get('PORT') or 5000
   app.run('0.0.0.0', port=port, debug=True)
